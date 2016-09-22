18a4ebffdca85619ec2e4ce5ef023dd682274996
Shanae <shanae@liquidinstruments.com>
2016-09-22 16:54:39 +1000

    PM-69: Cleaning up test fixtures

--- tests/test_oscilloscope.py
+++ tests/test_oscilloscope.py
@@ -3,54 +3,46 @@ import pytest, time
 from pymoku import Moku
 from pymoku.instruments import *
 from pymoku._oscilloscope import _OSC_SCREEN_WIDTH, _OSC_ADC_SMPS
-from pymoku._siggen import SG_MOD_NONE, SG_MOD_AMPL, SG_MOD_PHASE, SG_MOD_FREQ, SG_MODSOURCE_INT, SG_MODSOURCE_ADC, SG_MODSOURCE_DAC
+from pymoku._siggen import SG_MOD_NONE, SG_MOD_AMPL, SG_MOD_PHASE, SG_MOD_FREQ, SG_MODSOURCE_INT, SG_MODSOURCE_ADC, SG_MODSOURCE_DAC, SG_WAVE_SINE, SG_WAVE_SQUARE, SG_WAVE_TRIANGLE, SG_WAVE_DC
 import conftest
 import numpy
 
-SIGGEN_SINE 	= 0
-SIGGEN_SQUARE	= 1
-SIGGEN_RAMP 	= 2
-SIGGEN_DC		= 3
-
+# Assertion helpers
 def in_bounds(v, center, err):
 	return abs(v - center) < abs(err)
 
+def _is_rising(p1,p2):
+	if (p2-p1) > 0:
+		return True
+	else:
+		return False
+
+def _is_falling(p1,p2):
+	if (p2-p1) < 0:
+		return True
+	else:
+		return False
+
 class Test_Siggen:
-
-	# Set the timebase and check it correctly sets the decimation rate
-	@pytest.mark.parametrize("t1, t2", [
-		(0, 1),
-		(0, 1e-1),
-		(0, 1e-2),
-		(-1,1),
-		(-2,1),
-		(-0.02,1.2),
-		(-10,1e-6)
-		])
-	def _1test_timebase(self, base_instr, t1, t2):
-		base_instr.set_timebase(t1,t2)
-		base_instr.commit()
-		timebase_res = base_instr._get_timebase(base_instr.decimation_rate, base_instr.pretrigger, base_instr.render_deci, base_instr.offset)
-		print timebase_res
-
 	'''
-		Test the generated output waveforms are as expected
+		This class tests the correctness of the embedded signal generator 
 	'''
+
 	@pytest.mark.parametrize("ch, vpp, freq, offset, waveform", 
-		itertools.product([1,2],[0, 0.5, 1.0],[1e3, 1e6], [0, 0.3, 0.5], [SIGGEN_SINE, SIGGEN_SQUARE, SIGGEN_RAMP]))
+		itertools.product([1,2],[0, 0.5, 1.0],[1e3, 1e6], [0, 0.3, 0.5], [SG_WAVE_SINE, SG_WAVE_SQUARE, SG_WAVE_TRIANGLE]))
 	def test_waveform_amp(self, base_instr, ch, vpp, freq, offset, waveform):
-		# Generate an output sinewave and loop to input
-		# Ensure the amplitude is right
-		# Ensure the frequency seems correct as well
+		'''
+			Test the max/min amplitude of the waveforms are correct
+		'''
 
-		# Timebase should allow ~5 cycles of input wave
+		# Set timebase to allow for 5 cycles
 		if freq == 0:
-			tspan = 1.0 # a second
+			tspan = 1.0 # Set DC to 1 second
 		else:
 			tspan = (1.0/freq) * 5.0
 		base_instr.set_timebase(0,tspan)
 
-		# Loop back output as input source and trigger on it
+		# Loop DAC to input to measure the generated waveforms
 		base_instr.set_source(ch,OSC_SOURCE_DAC)
 		if(ch==1):
 			base_instr.set_trigger(OSC_TRIG_DA1, OSC_EDGE_RISING, 0)
@@ -58,18 +50,18 @@ class Test_Siggen:
 			base_instr.set_trigger(OSC_TRIG_DA2, OSC_EDGE_RISING, 0)
 
 		# Generate the desired waveform
-		if waveform == SIGGEN_SINE:
+		if waveform == SG_WAVE_SINE:
 			base_instr.synth_sinewave(ch, vpp, freq, offset)
-		elif waveform == SIGGEN_SQUARE:
+		elif waveform == SG_WAVE_SQUARE:
 			base_instr.synth_squarewave(ch, vpp, freq,offset=offset)
-		elif waveform == SIGGEN_RAMP:
+		elif waveform == SG_WAVE_TRIANGLE:
 			base_instr.synth_rampwave(ch, vpp, freq, offset=offset)
 		base_instr.commit()
 
 		# 5mV Tolerance on max/min values
 		tolerance = 0.005
 
-		# Get a few frames and test that the max amplitudes of the generated signals are within bounds
+		# Test amplitudes on a few frames worth of generated output
 		for _ in range(10):
 			frame = base_instr.get_frame()
 
@@ -79,24 +71,33 @@ class Test_Siggen:
 				ch_frame = frame.ch2
 
 			# For debugging the received frame
-			for y in ch_frame:
-				print y
+			# for y in ch_frame:
+			#	print y
 
+			# Get max/min amplitudes for each frame
 			maxval = max(x for x in ch_frame if x is not None)
 			minval = min(x for x in ch_frame if x is not None)
 
+			# Check max/min values are within tolerance bounds
 			assert in_bounds(maxval, (vpp/2.0)+offset, tolerance)
 			assert in_bounds(minval, (-1*(vpp/2.0) + offset), tolerance)
 
 	@pytest.mark.parametrize("ch, vpp, freq, waveform", 
-		itertools.product([1,2],[1.0],[1, 100, 1e3, 100e3, 1e6, 3e6],[SIGGEN_SINE, SIGGEN_SQUARE, SIGGEN_RAMP]))
+		itertools.product([1,2],[1.0],[100, 1e3, 100e3, 1e6, 3e6],[SG_WAVE_SINE, SG_WAVE_SQUARE, SG_WAVE_TRIANGLE]))
 	def test_waveform_freq(self, base_instr, ch, vpp, freq, waveform):
-		# Set timebase of 5 periods
+		'''
+			Test the frequency of generated waveforms
+
+			This is done by checking that the amplitude of generated signals is constant as we jump across multiple cycles
+			of the waveform.
+		'''
+		# Set timebase to allow for 5 cycles
 		number_periods = 5
 		period = (1.0/freq)
 		tspan = period * number_periods
 		base_instr.set_timebase(0,tspan)
 
+		# Loop DAC output to input for measurement
 		base_instr.set_source(ch,OSC_SOURCE_DAC)
 		base_instr.set_xmode(OSC_FULL_FRAME)
 		if(ch==1):
@@ -104,25 +105,28 @@ class Test_Siggen:
 		else:
 			base_instr.set_trigger(OSC_TRIG_DA2, OSC_EDGE_RISING, 0)
 
-		# Figure out the timebase of frames
+		# Get back actual timebase (this will not be precisely 5 cycles due to rounding)
 		(tstart, tend) = base_instr._get_timebase(base_instr.decimation_rate, base_instr.pretrigger, base_instr.render_deci, base_instr.offset)
 
-		time_per_smp = (tend-tstart)/_OSC_SCREEN_WIDTH 	# Timestep per sample
-		smps_per_period = period/time_per_smp 			# Number of samples before a period is reached
+		# Compute the approximate number of samples per waveform period
+		time_per_smp = (tend-tstart)/_OSC_SCREEN_WIDTH
+		smps_per_period = period/time_per_smp
 
-		# Test all waveform types
-		if waveform == SIGGEN_SINE:
+		# Generate the waveform type to be tested
+		# Define the offset into each frame that we should start measuring amplitudes from
+		if waveform == SG_WAVE_SINE:
 			base_instr.synth_sinewave(ch,vpp,freq,0.0)
 			start_xs = [0, int(smps_per_period/2), int(smps_per_period/3), int(smps_per_period/4), int(smps_per_period/8), int(3*smps_per_period/4)]
-		elif waveform == SIGGEN_SQUARE:
+		elif waveform == SG_WAVE_SQUARE:
 			base_instr.synth_squarewave(ch, vpp, freq)
+			# Don't start on a squarewave edge
 			start_xs = [int(smps_per_period/3), int(3*smps_per_period/4), int(2*smps_per_period/3), int(smps_per_period/8), int(7*smps_per_period/8)]
-		elif waveform == SIGGEN_RAMP:
+		elif waveform == SG_WAVE_TRIANGLE:
 			base_instr.synth_rampwave(ch, vpp, freq)
 			start_xs = [0, int(smps_per_period/2), int(smps_per_period/3), int(smps_per_period/4), int(smps_per_period/8), int(3*smps_per_period/4)]
 		base_instr.commit()
 
-		# 2% amplitude tolerance
+		# Allow 2% variation in amplitude
 		allowable_error = 0.02*vpp
 
 		# Workaround for ensuring we receive a valid waveform in the frame
@@ -141,23 +145,24 @@ class Test_Siggen:
 			if(ch==2):
 				ch_frame = frame.ch2
 
-			# Start checking at different points along the waveform
 			for start_x in start_xs:
-				# Amplitude expected at multiples of periods along the waveform
+				# First amplitude measurement of the waveform
 				expectedv = ch_frame[start_x]
 
-				# Skip along the waveform, 1 period at a time
+				# Skip along the waveform, 1 cycle at a time and check
+				# the amplitude matches the expected value.
 				for i in range(number_periods-1):
 					x = start_x + int(round(i*smps_per_period))
 
 					actualv = ch_frame[x]
 
 					# For debugging the received frame
-					for y in ch_frame:
-						print y
+					#for y in ch_frame:
+					#	print y
 
 					# Debugging info
-					print "Allowable tolerance: %.10f, Error: %.10f, Frame index: %d, Expected value: %.10f, Actual value: %.10f, Samples per period: %d, Render deci: %f" % (allowable_error, expectedv-actualv, x, expectedv, actualv, smps_per_period, base_instr.render_deci)
+					# print "Allowable tolerance: %.10f, Error: %.10f, Frame index: %d, Expected value: %.10f, Actual value: %.10f, Samples per period: %d, Render deci: %f" % (allowable_error, expectedv-actualv, x, expectedv, actualv, smps_per_period, base_instr.render_deci)
+					
 					# Check actual value is within tolerance
 					assert in_bounds(actualv, expectedv, allowable_error)
 
@@ -173,7 +178,6 @@ class Test_Siggen:
 		base_instr.synth_sinewave(1, 1.0, 10, 0)
 		base_instr.synth_sinewave(2, 1.0, 5, 0)
 		base_instr.synth_modulate(1, SG_MOD_AMPL, SG_MODSOURCE_INT, depth, frequency)
-		#base_instr.synth_modulate(ch, SG_MOD_AMPL, source, depth, frequency)
 		base_instr.commit()
 
 		# Get sampling frequency
@@ -184,91 +188,57 @@ class Test_Siggen:
 
 class Test_Trigger:
 	'''
-		We want this class to test everything around triggering settings for the oscilloscope
+		This class tests Trigger modes of the Oscilloscope
 	'''
-
-	@pytest.mark.parametrize("trig_ch, edge, amp", 
-		itertools.product([1,2],[OSC_EDGE_RISING, OSC_EDGE_FALLING, OSC_EDGE_BOTH],
-		[-0.1, 0.0, 0.1, 0.3]))
-	def test_triggered_amplitude(self, base_instr, trig_ch, edge, amp):
-		'''
-			Ensure that the start of the frame is the expected amplitude (within some error)
-		'''
-		# Set up the trigger signal
-		if trig_ch == 1:
-			base_instr.synth_sinewave(1,1.0,100.0,0)
-			base_instr.set_timebase(0,0.01)
-			base_instr.set_source(1, OSC_SOURCE_DAC)
-			base_instr.set_trigger(OSC_TRIG_DA1, edge, amp, hysteresis=0, hf_reject=False, mode=OSC_TRIG_NORMAL)
-		if trig_ch == 2:
-			base_instr.synth_sinewave(2,1.0,100.0,0)
-			base_instr.set_timebase(0,0.01)
-			base_instr.set_source(2, OSC_SOURCE_DAC)
-			base_instr.set_trigger(OSC_TRIG_DA2, edge, amp, hysteresis=0, hf_reject=False, mode=OSC_TRIG_NORMAL)
-
-		base_instr.commit()
-
-		allowable_error = 0.005 # Volts
-		for n in range(10):
-			frame = base_instr.get_frame(timeout=5)
-			if trig_ch == 1:
-				ch_frame = frame.ch1
-			elif trig_ch == 2:
-				ch_frame = frame.ch2
-			print "Start of frame value: %.2f" % (ch_frame[0])
-			assert in_bounds(ch_frame[0], amp, allowable_error)
-
 	
-	@pytest.mark.parametrize("trig_ch, edge, waveform",
-		itertools.product([1,2],[OSC_EDGE_RISING, OSC_EDGE_FALLING, OSC_EDGE_BOTH], [SIGGEN_SINE, SIGGEN_SQUARE, SIGGEN_RAMP]))
-	def test_triggered_edge(self, base_instr, trig_ch, edge, waveform):
-		half_idx = (_OSC_SCREEN_WIDTH / 2) - 1
-
-		def _is_rising(p1,p2):
-			if (p2-p1) > 0:
-				return True
-			else:
-				return False
-
-		def _is_falling(p1,p2):
-			if (p2-p1) < 0:
-				return True
-			else:
-				return False
+	@pytest.mark.parametrize("trig_ch, edge, trig_level, waveform",
+		itertools.product([1,2],
+			[OSC_EDGE_RISING, OSC_EDGE_FALLING, OSC_EDGE_BOTH], 
+			[-0.1, 0.0, 0.1, 0.3], 
+			[SG_WAVE_SINE, SG_WAVE_SQUARE, SG_WAVE_TRIANGLE]))
+	def test_triggered_edge(self, base_instr, trig_ch, edge, trig_level, waveform):
 		'''
-			Ensure the edge type looks right
+			Test the triggered edge type and level are correct
 		'''
-		# Test rising/falling edges
-		if waveform == SIGGEN_SINE:
-			base_instr.synth_sinewave(trig_ch, 1.0, 100, 0)
-		elif waveform == SIGGEN_SQUARE:
-			base_instr.synth_squarewave(trig_ch, 1.0, 100, 0)
-		elif waveform == SIGGEN_RAMP:
-			base_instr.synth_rampwave(trig_ch, 1.0, 100, 0)
+		# Set up the source signal to test triggering on
+		source_freq = 100 #Hz
+		source_vpp = 1.0
+		source_offset = 0.0
+		base_instr.set_source(trig_ch, OSC_SOURCE_DAC)
+		if waveform == SG_WAVE_SINE:
+			base_instr.synth_sinewave(trig_ch, source_vpp, source_freq, source_offset)
+		elif waveform == SG_WAVE_SQUARE:
+			base_instr.synth_squarewave(trig_ch, source_vpp, source_freq, source_offset)
+		elif waveform == SG_WAVE_TRIANGLE:
+			base_instr.synth_rampwave(trig_ch, source_vpp, source_freq, source_offset)
 		else:
 			print "Invalid waveform type"
 			assert False
 
+		# Set a symmetric timebase of ~10 cycles
+		base_instr.set_timebase(-5/source_freq,5/source_freq)
+
+		# Sample number of trigger point given a symmetric timebase
+		half_idx = (_OSC_SCREEN_WIDTH / 2) - 1
+
+		# Trigger on correct DAC channel
 		if trig_ch == 1:
-			base_instr.set_timebase(-0.01,0.01)
-			base_instr.set_source(1, OSC_SOURCE_DAC)
-			base_instr.set_trigger(OSC_TRIG_DA1, edge, 0.0, hysteresis=0, hf_reject=False, mode=OSC_TRIG_NORMAL)
+			base_instr.set_trigger(OSC_TRIG_DA1, edge, trig_level, hysteresis=0, hf_reject=False, mode=OSC_TRIG_NORMAL)
 		if trig_ch == 2:
-			base_instr.set_timebase(-0.01,0.01)
-			print base_instr.pretrigger, base_instr.render_deci, float(base_instr.pretrigger)/float(base_instr.render_deci)
-			base_instr.set_source(2, OSC_SOURCE_DAC)
-			base_instr.set_trigger(OSC_TRIG_DA2, edge, 0.0, hysteresis=0, hf_reject=False, mode=OSC_TRIG_NORMAL)
+			base_instr.set_trigger(OSC_TRIG_DA2, edge, trig_level, hysteresis=0, hf_reject=False, mode=OSC_TRIG_NORMAL)
 
 		base_instr.commit()
 
-		for _ in range(10):
+		for _ in range(5):
 			frame = base_instr.get_frame(timeout=5)
 			if trig_ch == 1:
 				ch_frame = frame.ch1
 			elif trig_ch == 2:
 				ch_frame = frame.ch2
 
-			print "Start of frame values: %s" % (ch_frame[half_idx - 4: half_idx+4])
+			# Check for correct level unless we are on a square edge
+			if not (waveform == SG_WAVE_SQUARE):
+				assert in_bounds(trig_level,ch_frame[half_idx], 0.005)
 
 			if(edge == OSC_EDGE_RISING):
 				assert _is_rising(ch_frame[half_idx-1],ch_frame[half_idx])
