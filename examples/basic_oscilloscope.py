b718c8305f6cab114aa7bf1d5c82cd3db6de8f02
Ben Nizette <benjamin.nizette@anu.edu.au>
2017-01-10 13:04:16 +1100

    PM-133: Split oscilloscope example in two, one basic, one full plotting

--- examples/basic_oscilloscope.py
+++ examples/basic_oscilloscope.py
@@ -1,59 +1,20 @@
 from pymoku import Moku
 from pymoku.instruments import *
-import time, logging
 
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter
-
-logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s::%(message)s')
-logging.getLogger('pymoku').setLevel(logging.DEBUG)
-
-# Use Moku.get_by_serial() or get_by_name() if you don't know the IP
+# Can directly call the constructor with an IP address, or use
+# get_by_name or get_by_serial for autodiscovery.
 m = Moku.get_by_name('Moku')
-
-i = m.discover_instrument()
-
-if i is None or i.type != 'oscilloscope':
-	print("No or wrong instrument deployed")
-	i = Oscilloscope()
-	m.attach_instrument(i)
-else:
-	print("Attached to existing Oscilloscope")
-	m.take_ownership()
+i = Oscilloscope()
+m.attach_instrument(i)
 
 try:
-	i.set_defaults()
-	i.set_source(1,OSC_SOURCE_ADC)
-	i.set_trigger(OSC_TRIG_CH1, OSC_EDGE_RISING, 0)
-	i.synth_sinewave(1,0.5,10,0)
-	i.set_timebase(1.0, 2)
+	# Span from -1s to 1s i.e. trigger point centred
+	i.set_timebase(-1, 1)
 	i.commit()
 
-	plt.ion()
-	plt.show()
-	plt.grid(b=True)
-	plt.ylim([-10, 10])
-	plt.xlim([0,1024])
-
-	# Get initial frame to set up plotting parameters
-	frame=i.get_frame()
-	line1, = plt.plot([])
-	line2, = plt.plot([])
-
-	ax = plt.gca()
-	ax.xaxis.set_major_formatter(FuncFormatter(frame.get_xaxis_fmt))
-	ax.yaxis.set_major_formatter(FuncFormatter(frame.get_yaxis_fmt))
-	ax.fmt_xdata = frame.get_xcoord_fmt
-	ax.fmt_ydata = frame.get_ycoord_fmt
-
-	while True:
-		frame = i.get_frame()
-		plt.pause(0.001)
-		line1.set_ydata(frame.ch1)
-		line2.set_ydata(frame.ch2)
-		line1.set_xdata(list(range(1024)))
-		line2.set_xdata(list(range(1024)))
+	# Get and print a single frame's worth of data (time series
+	# of voltage per channel)
+	print(i.get_frame())
 
 finally:
 	m.close()
