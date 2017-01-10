943c3d636101a7842620a6a6f0ed15f76279b6c8
Ben Nizette <benjamin.nizette@anu.edu.au>
2017-01-10 13:17:51 +1100

    PM-133: Split the specan example in to a genuinely basic one, and a full plotting example

--- examples/basic_specan.py
+++ examples/basic_specan.py
@@ -1,90 +1,19 @@
 from pymoku import Moku
 from pymoku.instruments import *
-import time, logging
 
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter
-
-logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s::%(message)s')
-logging.getLogger('pymoku').setLevel(logging.DEBUG)
-
-# Use Moku.get_by_serial() or get_by_name() if you don't know the IP
+# Create our Moku object and configure it to be a Spectrum Analyser.
+# The constructor can take an IP address directly, or you can use the
+# get_by_* functions to perform auto-discovery.
 m = Moku.get_by_name('example')
+i = SpecAn()
+m.attach_instrument(i)
 
-i = m.discover_instrument()
-if i is None or i.type != 'specan':
-	print("No or wrong instrument deployed")
-	i = SpecAn()
-	m.attach_instrument(i)
-else:
-	print("Attached to existing Spectrum Analyser")
-	m.take_ownership()
-
-#################################
-# BEGIN Instrument Configuration
-# ------------------------------
-# Set these parameters
-#################################
-# Set power scale to dBm
-dbm = True
-
-# Set window type {NONE, BH, FLATTOP, HANNING}
-windowType = i.window_type('NONE')
-
-# Set FFT frequency span (Hz)
-start_freq = 0e6
-stop_freq = 250e6
-#################################
-# END Instrument Configuration
-#################################
-
-i.set_dbmscale(dbm)
-i.set_window(windowType)
-i.set_span(start_freq, stop_freq)
-
-# Push all new configuration to the Moku device
+# DC to 100MHz span, apply changes
+i.set_span(0, 100000)
 i.commit()
 
-# Set up basic plot configurations
-line1, = plt.plot([])
-line2, = plt.plot([])
-plt.ion()
-plt.show()
-plt.grid(b=True)
-if(dbm):
-	plt.ylim([-200, 100])
-else:
-	plt.ylim([-0.5,1])
-plt.autoscale(axis='x',tight=True)
+# Get the scan results and print them out (power vs frequency, two channels)
+print(i.get_frame())
 
-try:
-	# Get an initial frame to set any frame-specific plot parameters
-	frame = i.get_frame()
-
-	# Format the x-axis as a frequency scale 
-	ax = plt.gca()
-	ax.xaxis.set_major_formatter(FuncFormatter(frame.get_xaxis_fmt))
-	ax.yaxis.set_major_formatter(FuncFormatter(frame.get_yaxis_fmt))
-	ax.fmt_xdata = frame.get_xcoord_fmt
-	ax.fmt_ydata = frame.get_ycoord_fmt
-
-	# Start drawing new frames
-	while True:
-		frame = i.get_frame()
-		plt.pause(0.001)
-
-		# Set the frame data for each channel plot
-		line1.set_ydata(frame.ch1)
-		line2.set_ydata(frame.ch2)
-		# Frequency axis shouldn't change, but to be sure
-		line1.set_xdata(frame.ch1_fs)
-		line2.set_xdata(frame.ch2_fs)
-		# Ensure the frequency axis is a tight fit
-		ax.relim()
-		ax.autoscale_view()
-
-		# Redraw the lines
-		plt.draw()
-finally:
-	m.close()
+# Close the connection to the Moku.
+m.close()
---
+++ examples/plotting_specan.py
@@ -0,0 +1,90 @@
+from pymoku import Moku
+from pymoku.instruments import *
+import time, logging
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+
+logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s::%(message)s')
+logging.getLogger('pymoku').setLevel(logging.DEBUG)
+
+# Use Moku.get_by_serial() or get_by_name() if you don't know the IP
+m = Moku.get_by_name('example')
+
+i = m.discover_instrument()
+if i is None or i.type != 'specan':
+	print("No or wrong instrument deployed")
+	i = SpecAn()
+	m.attach_instrument(i)
+else:
+	print("Attached to existing Spectrum Analyser")
+	m.take_ownership()
+
+#################################
+# BEGIN Instrument Configuration
+# ------------------------------
+# Set these parameters
+#################################
+# Set power scale to dBm
+dbm = True
+
+# Set window type {NONE, BH, FLATTOP, HANNING}
+windowType = i.window_type('NONE')
+
+# Set FFT frequency span (Hz)
+start_freq = 0e6
+stop_freq = 250e6
+#################################
+# END Instrument Configuration
+#################################
+
+i.set_dbmscale(dbm)
+i.set_window(windowType)
+i.set_span(start_freq, stop_freq)
+
+# Push all new configuration to the Moku device
+i.commit()
+
+# Set up basic plot configurations
+line1, = plt.plot([])
+line2, = plt.plot([])
+plt.ion()
+plt.show()
+plt.grid(b=True)
+if(dbm):
+	plt.ylim([-200, 100])
+else:
+	plt.ylim([-0.5,1])
+plt.autoscale(axis='x',tight=True)
+
+try:
+	# Get an initial frame to set any frame-specific plot parameters
+	frame = i.get_frame()
+
+	# Format the x-axis as a frequency scale 
+	ax = plt.gca()
+	ax.xaxis.set_major_formatter(FuncFormatter(frame.get_xaxis_fmt))
+	ax.yaxis.set_major_formatter(FuncFormatter(frame.get_yaxis_fmt))
+	ax.fmt_xdata = frame.get_xcoord_fmt
+	ax.fmt_ydata = frame.get_ycoord_fmt
+
+	# Start drawing new frames
+	while True:
+		frame = i.get_frame()
+		plt.pause(0.001)
+
+		# Set the frame data for each channel plot
+		line1.set_ydata(frame.ch1)
+		line2.set_ydata(frame.ch2)
+		# Frequency axis shouldn't change, but to be sure
+		line1.set_xdata(frame.ch1_fs)
+		line2.set_xdata(frame.ch2_fs)
+		# Ensure the frequency axis is a tight fit
+		ax.relim()
+		ax.autoscale_view()
+
+		# Redraw the lines
+		plt.draw()
+finally:
+	m.close()
