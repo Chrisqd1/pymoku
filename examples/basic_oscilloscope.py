e28f4586b4bf303f5fdb15a2e4000d6f2d956a81
Shanae King <shanae@liquidinstruments.com>
2017-04-23 21:06:46 +1000

    PM-190: Oscilloscope example script updated

--- examples/basic_oscilloscope.py
+++ examples/basic_oscilloscope.py
@@ -1,19 +1,29 @@
 from pymoku import Moku
 from pymoku.instruments import *
 
-# Can directly call the constructor with an IP address, or use
-# get_by_name or get_by_serial for autodiscovery.
-m = Moku.get_by_name('Moku')
-i = Oscilloscope()
-m.deploy_instrument(i)
+# Connect to your Moku device by using the device name
+# Alternatively, if you know the IP address or serial number use 
+# Moku('192.168.###.###') or Moku.get_by_serial('#####'), respectively
+m = Moku.get_by_name('Jet Fuel')
+i = m.discover_instrument()
+
+if i is None or i.type != 'Oscilloscope':
+	print("No or wrong instrument deployed")
+	i = Oscilloscope()
+	m.deploy_instrument(i)
+else:
+	print("Attached to existing Phasemeter")
+	m.take_ownership()
+
 
 try:
 	# Span from -1s to 1s i.e. trigger point centred
-	i.set_timebase(-1, 1)
+	i.set_timebase(-1,1)
 
-	# Get and print a single frame's worth of data (time series
+	# Get and print a single frame worth of data (time series
 	# of voltage per channel)
-	print(i.get_frame())
+	data = i.get_realtime_data(timeout=10)
+	print(data.ch1,data.ch2,data.time)
 
 finally:
 	m.close()
