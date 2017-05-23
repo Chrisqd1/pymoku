7d306acbcf7bee0ad5c529878061eaf700cb22a7
Ben Coughlan <ben.coughlan@cgsy.com.au>
2017-05-24 02:15:50 +1000

    Filled in most control regs and got a working demo

--- examples/basic_arbwavegen.py
+++ examples/basic_arbwavegen.py
@@ -1,6 +1,6 @@
 from pymoku import Moku
 from pymoku.instruments import *
-import struct, logging
+import struct, logging, math
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -13,9 +13,19 @@ console.setFormatter(formatter)
 console.setLevel(logging.INFO)
 logging.getLogger().addHandler(console)
 
+data = [0x0000, 0x1000, 0x2000, 0x3000, 0x4000,
+        0x4000, 0x3000, 0x2000, 0x1000, 0x0000,
+        (-0x1000) & 0xFFFF, (-0x1000) & 0xFFFF]
+
 with open('data.dat', 'wb') as f:
-	for i in range(LENGTH*2):
-		f.write(struct.pack('<i', i))
+	for d in data:
+		f.write(struct.pack('<i', d))
+	for i in range(LENGTH - len(data)):
+		f.write(struct.pack('<i', 0))
+
+	#channel 2
+	for i in range(LENGTH):
+		f.write(struct.pack('<i', 0))
 
 m = Moku('192.168.1.99')
 i = ArbWaveGen()
@@ -23,45 +33,27 @@ m.deploy_instrument(i)
 
 try:
 	i.set_defaults()
-	i.set_timebase(-1e-6, 3e-6)
-	i.mode = 0
-	i.interpolation = True
-	i.out1_frequency = 1.532e2
-	i.out1_enable = True
-	i.set_source(1, 'out', lmode='round')
-	i.set_trigger('out1', 'rising', 0.1, hysteresis=0.05, mode='normal')
-	i.set_xmode('fullframe')
+	i.mode1 = 3
+	i.interpolation1 = True
+	i.lut_length1 = len(data)-1
+	i.phase_modulo1 = 2**17 * (len(data) + 50) - 1
+	i.dead_value1 = -0x1000
+	i.phase_step1 = 2**15
+
+	i.mmap_access = True
 	i.commit()
 
-	# m._send_file('j', 'data.dat')
-	from subprocess import check_output
-	for n in range(8):
-		offset = 0x48000000 + (0x2000 * 4 * n)
-		# check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x0C00\"' % (offset + 0x7FF8), shell=True)
+	m._send_file('j', 'data.dat')
 
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x0400\"' % (offset + 0x00), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x0800\"' % (offset + 0x04), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x0C00\"' % (offset + 0x08), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x1000\"' % (offset + 0x0C), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x1400\"' % (offset + 0x10), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x1800\"' % (offset + 0x14), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x1C00\"' % (offset + 0x18), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x2000\"' % (offset + 0x1C), shell=True)
+	i.mmap_access = False
+	i.enable1 = True
+	i.commit()
 
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x1000\"' % (offset + 0x40), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x1000\"' % (offset + 0x44), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x1000\"' % (offset + 0x48), shell=True)
-
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x1000\"' % (offset + 0x50), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x0C00\"' % (offset + 0x54), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x0800\"' % (offset + 0x58), shell=True)
-		check_output('ssh root@192.168.1.99 \"/sbin/devmem 0x%08X 32 0x0400\"' % (offset + 0x5C), shell=True)
-
-	i.set_trigger('out1', 'rising', 0.1, hysteresis=0.05, mode='normal')
-	i.set_xmode('fullframe')
+	i.set_source(1, 'out', lmode='round')
+	i.set_trigger('out1', 'rising', 0.0, hysteresis=0.05, mode='normal')
+	i.set_timebase(-5e-8, 10e-8)
 	i.commit()
 	data = i.get_realtime_data(wait=True, timeout=10)
-
 	plt.plot(data.time, data.ch1)
 	plt.plot(data.time, data.ch2)
 	plt.show()
