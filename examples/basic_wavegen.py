c1c7c7d1887a7d00254451a3ffa13c0da9162976
Max Schwenke <maxschwenke1@gmail.com>
2017-10-26 16:50:44 +1100

    PM-336: reverted basic script

--- examples/basic_wavegen.py
+++ examples/basic_wavegen.py
@@ -13,8 +13,7 @@ import time
 
 # Connect to your Moku by its device name
 # Alternatively, use Moku.get_by_serial('#####') or Moku('192.168.###.###')
-#m = Moku.get_by_name('Bode')
-m = Moku('192.168.69.65')
+m = Moku.get_by_name('Moku')
 
 # Prepare the Signal Generator instrument
 i = WaveformGenerator()
@@ -23,19 +22,15 @@ i = WaveformGenerator()
 m.deploy_instrument(i)
 try:
 	# Generate a 1.0Vpp 1MHz Sinewave on Channel 1
-	i.gen_sinewave(1, 1.0, 50)
+	i.gen_sinewave(1, 1.0, 1e6)
 
 	# Generate a 1.0Vpp 2MHz Squarewave on Channel 2
 	# 30% Duty cycle, 10% Rise time, 10% Fall time
-	i.gen_squarewave(2, 1.0, 1, risetime=0.1, falltime=0.1, duty=0.3)
+	i.gen_squarewave(2, 1.0, 2e6, risetime=0.1, falltime=0.1, duty=0.3)
 
 	# Amplitude modulate the Channel 1 sinewave with another internally-generated sinewave.
 	# 100% modulation depth at 10Hz.
-	i.gen_modulate(1, 'amplitude', 'internal', 1, 1)
-
-	time.sleep(20)
-
-	i.set_trigger(1, mode = 'ncycle', ncycles = 1, trigger_source = 'dac', trigger_threshold = 0)
+	i.gen_modulate(1, 'amplitude', 'internal', 1, 10)
 
 finally:
 	m.close()
