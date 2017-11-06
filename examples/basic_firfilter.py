470a3d559ad716afddffa609fcc74d4dd425f0fe
Ben Coughlan <ben.coughlan@cgsy.com.au>
2017-11-06 17:23:15 +1100

    HG-1735 Basic FIR implementation working

---
+++ examples/basic_firfilter.py
@@ -0,0 +1,109 @@
+from pymoku import Moku
+from pymoku.instruments import FIRFilter
+import numpy as np
+from scipy import signal
+
+import matplotlib
+import matplotlib.pyplot as plt
+from math import floor
+
+FS = 125e6 / 500
+length = 500000
+amp = 2**12
+f0 = 1e3
+
+filt1 = signal.firls(101, [0.0, 50.0e3, 50.0e3, 100.0e3, 100.0e3, FS/2.0], [0.1, 0.1, 1.0, 1.0, 0.1, 0.1], fs=FS)
+# filt1 = [1.0] + [0.0]*20000 + [1.0]
+filt2 = signal.firls(101, [0.0, 50.0e3, 50.0e3, 100.0e3, 100.0e3, FS/2.0], [0.1, 0.1, 1.0, 1.0, 0.1, 0.1], fs=FS)
+# filt2 = [-1.0] + [0.0]*20000 + [-1.0]
+# filt2 = [1e-5] * 404
+# filt = [0.5**9]*8
+# filt2 = filt2 * 2.0**2
+# plt.plot(filt1)
+# plt.plot(filt2)
+# plt.show()
+# print filt
+# exit(0)
+
+# Connect to your Moku by its device name
+# Alternatively, use Moku.get_by_serial('#####') or Moku('192.168.###.###')
+m = Moku('192.168.69.181', load_instruments=False)
+
+# Prepare the ArbWaveformGenerator instrument
+i = FIRFilter()
+m.deploy_instrument(i)
+
+
+
+# filt = [int(round(2.0**17 * x)) for x in filt]
+# print filt
+# plt.plot(filt)
+
+# sig = np.random.random_integers(-amp, amp-1, length)
+# sig_filt = signal.convolve(sig, filt, mode='same', method='direct')
+
+# plt.plot(sig, 'b')
+# plt.plot(sig_filt, 'r')
+
+# psd = np.abs(np.fft.fft([x / 1.0 for x in sig_filt]))**2
+# plt.loglog(psd[0:len(psd)/2], 'r')
+
+# plt.loglog(psd[0], psd[1])
+# print sig_filt[:100]
+# plt.show()
+
+
+try:
+
+	i.set_defaults()
+	i._set_frontend(1, fiftyr=True, atten=False, ac=False)
+	i.set_trigger('in1', 'rising', 0.00, hysteresis=True)
+	i.set_timebase(-25e-6,400e-6)
+
+	i.set_source(1, 'in')
+	i.set_source(2, 'out')
+
+	i.decimation2 = 100
+	i.decimation1 = 100
+	i.upsampling1 = int(floor(2.0**17 / ((i.decimation1+1))))
+	i.write_coeffs(1, filt1)
+	i.upsampling2 = int(floor(2.0**17 / ((i.decimation2+1))))
+	i.write_coeffs(2, filt2)
+
+	i.link = True
+	i.commit()
+
+	i._set_mmap_access(True)
+	error = i._moku._receive_file('j', '.lutdata_moku.dat', 511 * 40 * 4 * 2)
+	i._set_mmap_access(False)
+
+	data = i.get_realtime_data()
+	# Set up the plotting parameters
+	plt.ion()
+	plt.show()
+	plt.grid(b=True)
+	plt.ylim([-0.1,0.1])
+	plt.xlim([data.time[0], data.time[-1]])
+
+	line1, = plt.plot([])
+	line2, = plt.plot([])
+
+	# This loops continuously updates the plot with new data
+	while True:
+		# Get new data
+		data = i.get_realtime_data()
+
+		# Update the plot
+		# psd = signal.welch(data.ch1, fs=FS)
+		# line1.set_ydata(psd[1])
+		# line1.set_xdata(psd[0])
+
+		line1.set_ydata(data.ch1)
+		line2.set_ydata(data.ch2)
+		line1.set_xdata(data.time)
+		line2.set_xdata(data.time)
+		plt.pause(0.001)
+
+
+finally:
+	m.close()
---
+++ pymoku/_firfilter.py
@@ -0,0 +1,113 @@
+
+import math
+import logging
+import os
+
+from ._instrument import *
+CHN_BUFLEN = 2**13
+from . import _frame_instrument
+from pymoku._oscilloscope import _CoreOscilloscope
+from . import _utils
+
+log = logging.getLogger(__name__)
+
+REG_FIR_DECIMATION1 = 96
+REG_FIR_DECIMATION2 = 97
+REG_FIR_IN_SCALE1 = 98
+REG_FIR_IN_SCALE2 = 99
+REG_FIR_IN_OFFSET1 = 100
+REG_FIR_IN_OFFSET2 = 101
+REG_FIR_OUT_SCALE1 = 102
+REG_FIR_OUT_SCALE2 = 103
+REG_FIR_OUT_OFFSET1 = 104
+REG_FIR_OUT_OFFSET2 = 105
+REG_FIR_UPSAMPLING1 = 106
+REG_FIR_UPSAMPLING2 = 107
+REG_FIR_LINK = 108
+
+_FIR_NUM_BLOCKS = 44
+_FIR_BLOCK_SIZE = 511
+
+class FIRFilter(_CoreOscilloscope):
+	"""
+	.. automethod:: pymoku.instruments.FIRFilter.__init__
+	"""
+
+	def __init__(self):
+		super(FIRFilter, self).__init__()
+		self._register_accessors(_fir_reg_handlers)
+		self.id = 10
+		self.type = "firfilter"
+
+	@needs_commit
+	def set_defaults(self):
+		super(FIRFilter, self).set_defaults()
+		self.input_scale1 = 0x0040
+		self.input_offset1 = 0x0000
+		self.input_scale2 = 0x0040
+		self.input_offset2 = 0x0000
+		self.output_scale1 = 0x0020
+		self.output_offset1 = 0x0000
+		self.output_scale2 = 0x0020
+		self.output_offset2 = 0x0000
+
+	def write_coeffs(self, ch, coeffs):
+		coeffs = list(coeffs)
+		_utils.check_parameter_valid('set', ch, [1,2],'output channel')
+		assert len(coeffs) <= _FIR_NUM_BLOCKS * _FIR_BLOCK_SIZE
+		L = int(math.floor(float(len(coeffs))/_FIR_NUM_BLOCKS))
+		blocks = [coeffs[x:x+L] for x in range(0, len(coeffs), L)]
+		print L
+		blocks += [[]] * (_FIR_NUM_BLOCKS - len(blocks))
+
+		print map(len, blocks)
+		if not os.path.exists('.lutdata.dat'):
+			open('.lutdata.dat', 'w').close()
+
+		with open('.lutdata.dat', 'r+b') as f:
+			#first check and make the file the right size
+			f.seek(0, os.SEEK_END)
+			size = f.tell()
+			f.write('\0'.encode(encoding='UTF-8') * (2**16 * 4  - size))
+			f.flush()
+
+			#Leave the previous data file so we just rewite the new part,
+			#as we have to upload both channels at once.
+			if ch == 1:
+				offset = 0
+			else:
+				offset = 2**15 * 4
+
+			#clear the current contents
+			f.seek(offset)
+			f.write('\x00\x00\x00\x00' * _FIR_BLOCK_SIZE * _FIR_NUM_BLOCKS)
+
+			for i, b in enumerate(blocks):
+				b.reverse()
+				for j, c in enumerate(b):
+					f.seek(offset + (i * (_FIR_BLOCK_SIZE+1) * 4) + (j * 4))
+					f.write(struct.pack('<i', math.ceil((2.0**17-1) * c)))
+				f.seek(offset + (i * (_FIR_BLOCK_SIZE+1) * 4) + (_FIR_BLOCK_SIZE * 4))
+				f.write(struct.pack('<I', len(b)))
+
+			f.flush()
+
+		self._set_mmap_access(True)
+		error = self._moku._send_file('j', '.lutdata.dat')
+		self._set_mmap_access(False)
+
+_fir_reg_handlers = {
+	'decimation1':			(REG_FIR_DECIMATION1,		to_reg_unsigned(0, 32), from_reg_unsigned(0, 32)),
+	'decimation2':			(REG_FIR_DECIMATION2,		to_reg_unsigned(0, 32), from_reg_unsigned(0, 32)),
+	'input_scale1':			(REG_FIR_IN_SCALE1,		to_reg_unsigned(0, 18), from_reg_unsigned(0, 18)),
+	'input_scale2':			(REG_FIR_IN_SCALE2,		to_reg_unsigned(0, 18), from_reg_unsigned(0, 18)),
+	'input_offset1':			(REG_FIR_IN_OFFSET1,		to_reg_unsigned(0, 16), from_reg_unsigned(0, 16)),
+	'input_offset2':			(REG_FIR_IN_OFFSET2,		to_reg_unsigned(0, 16), from_reg_unsigned(0, 16)),
+	'output_scale1':			(REG_FIR_OUT_SCALE1,		to_reg_unsigned(0, 18), from_reg_unsigned(0, 18)),
+	'output_scale2':			(REG_FIR_OUT_SCALE2,		to_reg_unsigned(0, 18), from_reg_unsigned(0, 18)),
+	'output_offset1':			(REG_FIR_OUT_OFFSET1,		to_reg_unsigned(0, 16), from_reg_unsigned(0, 16)),
+	'output_offset2':			(REG_FIR_OUT_OFFSET2,		to_reg_unsigned(0, 16), from_reg_unsigned(0, 16)),
+	'upsampling1':			(REG_FIR_UPSAMPLING1,		to_reg_unsigned(0, 14), from_reg_unsigned(0, 14)),
+	'upsampling2':			(REG_FIR_UPSAMPLING2,		to_reg_unsigned(0, 14), from_reg_unsigned(0, 14)),
+	'link':			(REG_FIR_LINK,		to_reg_bool(0), from_reg_bool(0)),
+}
