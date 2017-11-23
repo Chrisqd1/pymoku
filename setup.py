5d78b37d052f9d541d8a12457cbd14b6f007b5e4
Ben Nizette <benjamin.nizette@anu.edu.au>
2017-11-23 11:17:52 +1100

    PM-376: Add MANIFEST.in that knows about our extension header files

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, Extension
 from pkg_resources import resource_filename, resource_isdir
 from tempfile import mkstemp
 from zipfile import ZipFile
+from glob import glob
 
 version = open('pymoku/version.txt').read().strip()
 
@@ -15,18 +16,7 @@ j = os.path.join
 lr_ext = Extension(
 	'lr',
 	include_dirs=['liquidreader'],
-	sources=[
-		j('liquidreader','bitcpy.c'),
-		j('liquidreader','lireader.c'),
-		j('liquidreader','liparse.c'),
-		j('liquidreader','liutility.c'),
-		j('liquidreader','linumber.c'),
-		j('liquidreader','capn.c'),
-		j('liquidreader','capn-malloc.c'),
-		j('liquidreader','capn-stream.c'),
-		j('liquidreader','li.capnp.c'),
-		j('lr_mod','lr_module.c'),
-	],
+	sources=glob('liquidreader/*.c') + glob('lr_mod/*.c'),
 	extra_compile_args=['-std=c99'],
 )
 
