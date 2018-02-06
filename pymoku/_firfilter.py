49b511c7e44acd7fd924951344ba57e8f966237d
Ben Coughlan <ben.coughlan@cgsy.com.au>
2018-02-06 12:15:59 +1100

    HG-1735 Refactored decimation filters into seperate class

--- pymoku/_firfilter.py
+++ pymoku/_firfilter.py
@@ -10,34 +10,107 @@ from . import _utils
 
 log = logging.getLogger(__name__)
 
-REG_FIR_DECIMATION_CH0 = 96
-REG_FIR_IN_SCALE1 = 98
-REG_FIR_IN_OFFSET1 = 100
-REG_FIR_OUT_SCALE1 = 102
-REG_FIR_OUT_OFFSET1 = 104
-REG_FIR_INTERPOLATION_CH0_WDFRATES 	= 106
-REG_FIR_INTERPOLATION_CH0_CICRATES 	= 107
-REG_FIR_INTERPOLATION_CH0_CTRL 		= 108
+REG_FIR_CONTROL = 96
 
-REG_FIR_DECIMATION_CH1 = 97
-REG_FIR_IN_SCALE2 = 99
-REG_FIR_IN_OFFSET2 = 101
-REG_FIR_OUT_SCALE2 = 103
-REG_FIR_OUT_OFFSET2 = 105
-REG_FIR_INTERPOLATION_CH1_WDFRATES 	= 109
-REG_FIR_INTERPOLATION_CH1_CICRATES 	= 110
-REG_FIR_INTERPOLATION_CH1_CTRL 		= 111
+REG_FIR_IN_SCALE1 = 105
+REG_FIR_IN_OFFSET1 = 106
+REG_FIR_OUT_SCALE1 = 107
+REG_FIR_OUT_OFFSET1 = 108
+REG_FIR_MATRIXGAIN_CH1 = 109
 
-REG_FIR_LINK = 112
-REG_FIR_RESET_CH1 = 113
-REG_FIR_RESET_CH2 = 114
-REG_FIR_MATRIXGAIN_CH1 = 115
-REG_FIR_MATRIXGAIN_CH2 = 116
-REG_FIR_OUTPUTENABLE = 117
-REG_FIR_MONSELECT = 118
+REG_FIR_IN_SCALE2 = 110
+REG_FIR_IN_OFFSET2 = 111
+REG_FIR_OUT_SCALE2 = 112
+REG_FIR_OUT_OFFSET2 = 113
+REG_FIR_MATRIXGAIN_CH2 = 114
 
 _FIR_NUM_BLOCKS = 29
 _FIR_BLOCK_SIZE = 511
+
+class _DecFilter(object):
+	REG_DECIMATION = 0
+	REG_INTERP_WDFRATES = 1
+	REG_INTERP_CICRATES = 2
+	REG_INTERP_CTRL = 3
+
+	def __init__(self, instr, regbase):
+		self._instr = instr
+		self.regbase = regbase
+
+	def set_samplerate(self, factor):
+		d_wdfmuxsel = 0
+		d_outmuxsel = 0
+		d_cic1_dec = 0
+		d_cic1_bitshift = 0
+		d_cic2_dec = 0
+		d_cic2_bitshift = 0
+		i_muxsel = 0
+		i_ratechange_cic2 = 0
+		i_interprate_cic2 = 0
+		i_bitshift_cic2 = 0
+		i_ratechange_cic1 = 0
+		i_bitshift_cic1 = 0
+		i_interprate_cic1 = 0
+		i_highrate_wdf1 = 0
+		i_highrate_wdf2 = 0
+		if factor == 1:
+			d_outmuxsel = 0
+			i_muxsel = 0
+		elif factor == 2:
+			d_wdfmuxsel = 0
+			d_outmuxsel = 1
+			i_muxsel = 1
+			i_highrate_wdf1 = 0
+		elif factor == 4:
+			d_wdfmuxsel = 0
+			d_outmuxsel = 2
+			i_muxsel = 2
+			i_highrate_wdf1 = factor/2 - 1
+			i_highrate_wdf2 = 0
+		elif 8 <= factor <= 64:
+			d_wdfmuxsel = 1
+			d_outmuxsel = 2
+			d_cic1_dec = factor/4
+			d_cic1_bitshift = 12 - math.log(d_cic1_dec**3,2)
+			i_muxsel = 3
+			i_ratechange_cic1 = factor/4
+			i_interprate_cic1 = 0
+			i_bitshift_cic1 = math.log(i_ratechange_cic1**2,2)
+			i_highrate_wdf1 = factor/2 - 1
+			i_highrate_wdf2 = factor/4 - 1
+		else: # 128 <= factor <= 1024
+			d_wdfmuxsel = 2
+			d_outmuxsel = 2
+			d_cic1_dec = 16
+			d_cic1_bitshift = 0
+			d_cic2_dec = factor/64
+			d_cic2_bitshift = math.log(d_cic2_dec**3,2)
+			i_muxsel = 4
+			i_ratechange_cic2 = factor/64
+			i_interprate_cic2 = 0
+			i_bitshift_cic2 = math.log(i_ratechange_cic2**2,2)
+			i_ratechange_cic1 = 16
+			i_bitshift_cic1 = 8
+			i_interprate_cic1 = i_ratechange_cic2 - 1
+			i_highrate_wdf1 = factor/2 - 1
+			i_highrate_wdf2 = factor/4 - 1
+
+		self._instr._accessor_set(self.regbase + self.REG_DECIMATION, to_reg_unsigned(0, 2), d_wdfmuxsel)
+		self._instr._accessor_set(self.regbase + self.REG_DECIMATION, to_reg_unsigned(2, 2), d_outmuxsel)
+		self._instr._accessor_set(self.regbase + self.REG_DECIMATION, to_reg_unsigned(4, 4), d_cic1_bitshift)
+		self._instr._accessor_set(self.regbase + self.REG_DECIMATION, to_reg_unsigned(8, 4), d_cic2_bitshift)
+		self._instr._accessor_set(self.regbase + self.REG_DECIMATION, to_reg_unsigned(12, 5), d_cic1_dec)
+		self._instr._accessor_set(self.regbase + self.REG_DECIMATION, to_reg_unsigned(17, 5), d_cic2_dec)
+
+		self._instr._accessor_set(self.regbase + self.REG_INTERP_CTRL, to_reg_unsigned(0, 3), i_muxsel)
+		self._instr._accessor_set(self.regbase + self.REG_INTERP_WDFRATES, to_reg_unsigned(0, 16), i_highrate_wdf1)
+		self._instr._accessor_set(self.regbase + self.REG_INTERP_WDFRATES, to_reg_unsigned(16, 16), i_highrate_wdf2)
+		self._instr._accessor_set(self.regbase + self.REG_INTERP_CTRL, to_reg_unsigned(3, 5), i_ratechange_cic1)
+		self._instr._accessor_set(self.regbase + self.REG_INTERP_CTRL, to_reg_unsigned(8, 5), i_ratechange_cic2)
+		self._instr._accessor_set(self.regbase + self.REG_INTERP_CICRATES, to_reg_unsigned(0, 16), i_interprate_cic1)
+		self._instr._accessor_set(self.regbase + self.REG_INTERP_CICRATES, to_reg_unsigned(16, 16), i_interprate_cic2)
+		self._instr._accessor_set(self.regbase + self.REG_INTERP_CTRL, to_reg_unsigned(13, 4), i_bitshift_cic1)
+		self._instr._accessor_set(self.regbase + self.REG_INTERP_CTRL, to_reg_unsigned(17, 4), i_bitshift_cic2)
 
 class FIRFilter(_CoreOscilloscope):
 	"""
@@ -49,6 +122,9 @@ class FIRFilter(_CoreOscilloscope):
 		self._register_accessors(_fir_reg_handlers)
 		self.id = 10
 		self.type = "firfilter"
+
+		self._decfilter1 = _DecFilter(self, 97)
+		self._decfilter2 = _DecFilter(self, 101)
 
 	@needs_commit
 	def set_defaults(self):
@@ -138,7 +214,7 @@ class FIRFilter(_CoreOscilloscope):
 			self.matrixscale_ch2_ch1 = control_matrix_ch1
 			self.matrixscale_ch2_ch2 = control_matrix_ch2
 
-	def set_filter(self, ch, decimation_factor, filter_coefficients=None, enable = True, link = False):
+	def set_filter(self, ch, decimation_factor, filter_coefficients, enable=True, link=False):
 		"""
 		Set FIR filter sample rate and coefficients and toggle channel output on/off.
 
@@ -165,108 +241,20 @@ class FIRFilter(_CoreOscilloscope):
 		self._write_coeffs(ch, filter_coefficients)
 
 	@needs_commit
+	def _set_samplerate(self, ch, factor):
+		if ch == 1:
+			self._decfilter1.set_samplerate(factor)
+		else:
+			self._decfilter2.set_samplerate(factor)
+		self._channel_reset(ch)
+
+	@needs_commit
 	def _set_output_link(self, ch, enable, link):
 		self.link = link
 		if ch == 1:
 			self.ch1_output = enable
 		else:
 			self.ch2_output = enable
-
-	@needs_commit
-	def _set_samplerate(self, ch, decimation_factor = 8):
-		self._channel_reset(ch)
-
-		d_wdfmuxsel = 0
-		d_outmuxsel = 0
-		d_cic1_dec = 0
-		d_cic1_bitshift = 0
-		d_cic2_dec = 0
-		d_cic2_bitshift = 0
-		i_muxsel = 0
-		i_ratechange_cic2 = 0
-		i_interprate_cic2 = 0
-		i_bitshift_cic2 = 0
-		i_ratechange_cic1 = 0
-		i_bitshift_cic1 = 0
-		i_interprate_cic1 = 0
-		i_highrate_wdf1 = 0
-		i_highrate_wdf2 = 0
-		if decimation_factor == 1:
-			d_outmuxsel = 0
-			i_muxsel = 0
-		elif decimation_factor == 2:
-			d_wdfmuxsel = 0
-			d_outmuxsel = 1
-			i_muxsel = 1
-			i_highrate_wdf1 = 0
-		elif decimation_factor == 4:
-			d_wdfmuxsel = 0
-			d_outmuxsel = 2
-			i_muxsel = 2
-			i_highrate_wdf1 = decimation_factor/2 - 1
-			i_highrate_wdf2 = 0
-		elif 8 <= decimation_factor <= 64:
-			d_wdfmuxsel = 1
-			d_outmuxsel = 2
-			d_cic1_dec = decimation_factor/4
-			d_cic1_bitshift = 12 - math.log(d_cic1_dec**3,2)
-			i_muxsel = 3
-			i_ratechange_cic1 = decimation_factor/4
-			i_interprate_cic1 = 0
-			i_bitshift_cic1 = math.log(i_ratechange_cic1**2,2)
-			i_highrate_wdf1 = decimation_factor/2 - 1
-			i_highrate_wdf2 = decimation_factor/4 - 1
-		else: # 128 <= decimation_factor <= 1024
-			d_wdfmuxsel = 2
-			d_outmuxsel = 2
-			d_cic1_dec = 16
-			d_cic1_bitshift = 0
-			d_cic2_dec = decimation_factor/64
-			d_cic2_bitshift = math.log(d_cic2_dec**3,2)
-			i_muxsel = 4
-			i_ratechange_cic2 = decimation_factor/64
-			i_interprate_cic2 = 0
-			i_bitshift_cic2 = math.log(i_ratechange_cic2**2,2)
-			i_ratechange_cic1 = 16
-			i_bitshift_cic1 = 8
-			i_interprate_cic1 = i_ratechange_cic2 - 1
-			i_highrate_wdf1 = decimation_factor/2 - 1
-			i_highrate_wdf2 = decimation_factor/4 - 1
-
-		if ch == 1:
-			self.dec_wdfmuxsel_1 = d_wdfmuxsel
-			self.dec_outmuxsel_1 = d_outmuxsel
-			self.dec_cic1_dec_1 = d_cic1_dec
-			self.dec_cic1_bitshift_1 = d_cic1_bitshift
-			self.dec_cic2_dec_1 = d_cic2_dec
-			self.dec_cic2_bitshift_1 = d_cic2_bitshift
-
-			self.int_muxsel_1 = i_muxsel
-			self.int_highrate_wdf1_1 = i_highrate_wdf1
-			self.int_highrate_wdf2_1 = i_highrate_wdf2
-			self.int_ratechange_cic1_1 = i_ratechange_cic1
-			self.int_ratechange_cic2_1 = i_ratechange_cic2
-			self.int_interprate_cic1_1 = i_interprate_cic1
-			self.int_interprate_cic2_1 = i_interprate_cic2
-			self.int_bitshift_cic1_1 = i_bitshift_cic1
-			self.int_bitshift_cic2_1 = i_bitshift_cic2
-		else:
-			self.dec_wdfmuxsel_2 = d_wdfmuxsel
-			self.dec_outmuxsel_2 = d_outmuxsel
-			self.dec_cic1_dec_2 = d_cic1_dec
-			self.dec_cic1_bitshift_2 = d_cic1_bitshift
-			self.dec_cic2_dec_2 = d_cic2_dec
-			self.dec_cic2_bitshift_2 = d_cic2_bitshift
-
-			self.int_muxsel_2 = i_muxsel
-			self.int_highrate_wdf1_2 = i_highrate_wdf1
-			self.int_highrate_wdf2_2 = i_highrate_wdf2
-			self.int_ratechange_cic1_2 = i_ratechange_cic1
-			self.int_ratechange_cic2_2 = i_ratechange_cic2
-			self.int_interprate_cic1_2 = i_interprate_cic1
-			self.int_interprate_cic2_2 = i_interprate_cic2
-			self.int_bitshift_cic1_2 = i_bitshift_cic1
-			self.int_bitshift_cic2_2 = i_bitshift_cic2
 
 	def _channel_reset(self, ch):
 		if ch == 1:
@@ -318,57 +306,23 @@ class FIRFilter(_CoreOscilloscope):
 		self._set_mmap_access(False)
 
 _fir_reg_handlers = {
-	'input_scale1':			(REG_FIR_IN_SCALE1,		to_reg_unsigned(0, 18), from_reg_unsigned(0, 18)),
-	'input_scale2':			(REG_FIR_IN_SCALE2,		to_reg_unsigned(0, 18), from_reg_unsigned(0, 18)),
-	'input_offset1':			(REG_FIR_IN_OFFSET1,		to_reg_signed(0, 16), from_reg_signed(0, 16)),
-	'input_offset2':			(REG_FIR_IN_OFFSET2,		to_reg_signed(0, 16), from_reg_signed(0, 16)),
-	'output_scale1':			(REG_FIR_OUT_SCALE1,		to_reg_unsigned(0, 18), from_reg_unsigned(0, 18)),
-	'output_scale2':			(REG_FIR_OUT_SCALE2,		to_reg_unsigned(0, 18), from_reg_unsigned(0, 18)),
-	'output_offset1':			(REG_FIR_OUT_OFFSET1,		to_reg_unsigned(0, 16), from_reg_unsigned(0, 16)),
-	'output_offset2':			(REG_FIR_OUT_OFFSET2,		to_reg_unsigned(0, 16), from_reg_unsigned(0, 16)),
-	'link':			(REG_FIR_LINK,		to_reg_bool(0), from_reg_bool(0)),
+	'reset_ch1':			(REG_FIR_CONTROL,			to_reg_bool(0), from_reg_bool(0)),
+	'reset_ch2':			(REG_FIR_CONTROL,			to_reg_bool(1), from_reg_bool(1)),
+	'ch1_output':			(REG_FIR_CONTROL,			to_reg_bool(2), from_reg_bool(2)),
+	'ch2_output':			(REG_FIR_CONTROL,			to_reg_bool(3), from_reg_bool(3)),
+	'link':					(REG_FIR_CONTROL,			to_reg_bool(4), from_reg_bool(4)),
 
-	'dec_wdfmuxsel_1':	(REG_FIR_DECIMATION_CH0,		to_reg_unsigned(0, 2), from_reg_unsigned(0, 2)),
-	'dec_outmuxsel_1':	(REG_FIR_DECIMATION_CH0,		to_reg_unsigned(2, 2), from_reg_unsigned(2, 2)),
-	'dec_cic1_bitshift_1':		(REG_FIR_DECIMATION_CH0,		to_reg_unsigned(4, 4), from_reg_unsigned(4, 4)),
-	'dec_cic2_bitshift_1':		(REG_FIR_DECIMATION_CH0,		to_reg_unsigned(8, 4), from_reg_unsigned(8, 4)),
-	'dec_cic1_dec_1':		(REG_FIR_DECIMATION_CH0,		to_reg_unsigned(12, 5), from_reg_unsigned(12, 5)),
-	'dec_cic2_dec_1':		(REG_FIR_DECIMATION_CH0,		to_reg_unsigned(17, 5), from_reg_unsigned(17, 5)),
+	'input_scale1':			(REG_FIR_IN_SCALE1,			to_reg_signed(0, 18), from_reg_signed(0, 18)),
+	'input_scale2':			(REG_FIR_IN_SCALE2,			to_reg_signed(0, 18), from_reg_signed(0, 18)),
+	'input_offset1':		(REG_FIR_IN_OFFSET1,		to_reg_signed(0, 32), from_reg_signed(0, 32)),
+	'input_offset2':		(REG_FIR_IN_OFFSET2,		to_reg_signed(0, 32), from_reg_signed(0, 32)),
+	'output_scale1':		(REG_FIR_OUT_SCALE1,		to_reg_signed(0, 18), from_reg_signed(0, 18)),
+	'output_scale2':		(REG_FIR_OUT_SCALE2,		to_reg_signed(0, 18), from_reg_signed(0, 18)),
+	'output_offset1':		(REG_FIR_OUT_OFFSET1,		to_reg_signed(0, 32), from_reg_signed(0, 32)),
+	'output_offset2':		(REG_FIR_OUT_OFFSET2,		to_reg_signed(0, 32), from_reg_signed(0, 32)),
 
-	'dec_wdfmuxsel_2':	(REG_FIR_DECIMATION_CH1,		to_reg_unsigned(0, 2), from_reg_unsigned(0, 2)),
-	'dec_outmuxsel_2':	(REG_FIR_DECIMATION_CH1,		to_reg_unsigned(2, 2), from_reg_unsigned(2, 2)),
-	'dec_cic1_bitshift_2':		(REG_FIR_DECIMATION_CH1,		to_reg_unsigned(4, 4), from_reg_unsigned(4, 4)),
-	'dec_cic2_bitshift_2':		(REG_FIR_DECIMATION_CH1,		to_reg_unsigned(8, 4), from_reg_unsigned(8, 4)),
-	'dec_cic1_dec_2':		(REG_FIR_DECIMATION_CH1,		to_reg_unsigned(12, 5), from_reg_unsigned(12, 5)),
-	'dec_cic2_dec_2':		(REG_FIR_DECIMATION_CH1,		to_reg_unsigned(17, 5), from_reg_unsigned(17, 5)),
-
-	'int_highrate_wdf1_1': 	(REG_FIR_INTERPOLATION_CH0_WDFRATES, 	to_reg_unsigned(0,16), from_reg_unsigned(0,16)),
-	'int_highrate_wdf2_1': 	(REG_FIR_INTERPOLATION_CH0_WDFRATES, 	to_reg_unsigned(16,16), from_reg_unsigned(16,16)),
-	'int_interprate_cic1_1': 	(REG_FIR_INTERPOLATION_CH0_CICRATES, 	to_reg_unsigned(0,16), from_reg_unsigned(0,16)),
-	'int_interprate_cic2_1': 	(REG_FIR_INTERPOLATION_CH0_CICRATES, 	to_reg_unsigned(16,16), from_reg_unsigned(16,16)),
-	'int_muxsel_1': 			(REG_FIR_INTERPOLATION_CH0_CTRL, 	to_reg_unsigned(0,3), from_reg_unsigned(0,3)),
-	'int_ratechange_cic1_1':	(REG_FIR_INTERPOLATION_CH0_CTRL, 	to_reg_unsigned(3,5), from_reg_unsigned(3,5)),
-	'int_ratechange_cic2_1':	(REG_FIR_INTERPOLATION_CH0_CTRL, 	to_reg_unsigned(8,5), from_reg_unsigned(8,5)),
-	'int_bitshift_cic1_1':	(REG_FIR_INTERPOLATION_CH0_CTRL, 	to_reg_unsigned(13,4), from_reg_unsigned(13,4)),
-	'int_bitshift_cic2_1':	(REG_FIR_INTERPOLATION_CH0_CTRL, 	to_reg_unsigned(17,4), from_reg_unsigned(17,4)),
-
-	'int_highrate_wdf1_2': 	(REG_FIR_INTERPOLATION_CH1_WDFRATES, 	to_reg_unsigned(0,16), from_reg_unsigned(0,16)),
-	'int_highrate_wdf2_2': 	(REG_FIR_INTERPOLATION_CH1_WDFRATES, 	to_reg_unsigned(16,16), from_reg_unsigned(16,16)),
-	'int_interprate_cic1_2': 	(REG_FIR_INTERPOLATION_CH1_CICRATES, 	to_reg_unsigned(0,16), from_reg_unsigned(0,16)),
-	'int_interprate_cic2_2': 	(REG_FIR_INTERPOLATION_CH1_CICRATES, 	to_reg_unsigned(16,16), from_reg_unsigned(16,16)),
-	'int_muxsel_2': 			(REG_FIR_INTERPOLATION_CH1_CTRL, 	to_reg_unsigned(0,3), from_reg_unsigned(0,3)),
-	'int_ratechange_cic1_2':	(REG_FIR_INTERPOLATION_CH1_CTRL, 	to_reg_unsigned(3,5), from_reg_unsigned(3,5)),
-	'int_ratechange_cic2_2':	(REG_FIR_INTERPOLATION_CH1_CTRL, 	to_reg_unsigned(8,5), from_reg_unsigned(8,5)),
-	'int_bitshift_cic1_2':	(REG_FIR_INTERPOLATION_CH1_CTRL, 	to_reg_unsigned(13,4), from_reg_unsigned(13,4)),
-	'int_bitshift_cic2_2':	(REG_FIR_INTERPOLATION_CH1_CTRL, 	to_reg_unsigned(17,4), from_reg_unsigned(17,4)),
-
-	'matrixscale_ch1_ch1': (REG_FIR_MATRIXGAIN_CH1,	to_reg_signed(0,16), from_reg_signed(0,16)),
-	'matrixscale_ch1_ch2': (REG_FIR_MATRIXGAIN_CH1,	to_reg_signed(16,16), from_reg_signed(16,16)),
-	'matrixscale_ch2_ch1': (REG_FIR_MATRIXGAIN_CH2,	to_reg_signed(0,16), from_reg_signed(0,16)),
-	'matrixscale_ch2_ch2': (REG_FIR_MATRIXGAIN_CH2,	to_reg_signed(16,16), from_reg_signed(16,16)),
-
-	'reset_ch1':	(REG_FIR_RESET_CH1, to_reg_unsigned(0,1), from_reg_unsigned(0,1)),
-	'reset_ch2':	(REG_FIR_RESET_CH2, to_reg_unsigned(0,1), from_reg_unsigned(0,1)),
-	'ch1_output':		(REG_FIR_OUTPUTENABLE,			to_reg_bool(0), from_reg_bool(0)),
-	'ch2_output':		(REG_FIR_OUTPUTENABLE,			to_reg_bool(1), from_reg_bool(1))
+	'matrixscale_ch1_ch1':	(REG_FIR_MATRIXGAIN_CH1,	to_reg_signed(0, 16), from_reg_signed(0, 16)),
+	'matrixscale_ch1_ch2':	(REG_FIR_MATRIXGAIN_CH1,	to_reg_signed(16, 16), from_reg_signed(16, 16)),
+	'matrixscale_ch2_ch1':	(REG_FIR_MATRIXGAIN_CH2,	to_reg_signed(0, 16), from_reg_signed(0, 16)),
+	'matrixscale_ch2_ch2':	(REG_FIR_MATRIXGAIN_CH2,	to_reg_signed(16, 16), from_reg_signed(16, 16))
 }
