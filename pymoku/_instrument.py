c4ea3c4a975910cff06829746cc247fafc9a1476
Tim <tim@liquidinstruments.com>
2018-06-19 16:01:42 +1000

    HG-2694 : reduced the scaling coefficients of the temprature coefficients....

--- pymoku/_instrument.py
+++ pymoku/_instrument.py
@@ -662,12 +662,12 @@ _instr_reg_handlers = {
 										from_reg_signed(16, 16, xform=lambda obj, x: round(x / 2.0**13))),
 
 	'dac1_offset':		(REG_CAL_DAC0,	to_reg_signed(0, 11),		from_reg_signed(0, 11)),
-	'dac1_offset_t':	(REG_CAL_DAC0,	to_reg_signed(16, 16, xform=lambda obj, x: x * 2.0**13),
-										from_reg_signed(16, 16, xform=lambda obj, x: x / 2.0**13)),
+	'dac1_offset_t':	(REG_CAL_DAC0,	to_reg_signed(16, 16, xform=lambda obj, x: x * 2.0**12),
+										from_reg_signed(16, 16, xform=lambda obj, x: x / 2.0**12)),
 
 	'dac2_offset':		(REG_CAL_DAC1,	to_reg_signed(0, 11),		from_reg_signed(0, 11)),
-	'dac2_offset_t':	(REG_CAL_DAC1,	to_reg_signed(16, 16, xform=lambda obj, x: x * 2.0**13),
-										from_reg_signed(16, 16, xform=lambda obj, x: x / 2.0**13)),
+	'dac2_offset_t':	(REG_CAL_DAC1,	to_reg_signed(16, 16, xform=lambda obj, x: x * 2.0**12),
+										from_reg_signed(16, 16, xform=lambda obj, x: x / 2.0**12)),
 
 	'state_id':			(REG_STATE,	 	to_reg_unsigned(0, 8),		from_reg_unsigned(0, 8)),
 	'state_id_alt':		(REG_STATE,	 	to_reg_unsigned(16, 8),		from_reg_unsigned(16, 8)),
