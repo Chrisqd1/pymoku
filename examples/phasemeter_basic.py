4272618dd7c7e11b1687fa2145222c80e0c00f8c
Ben Coughlan <ben@liquidinstruments.com>
2019-02-01 14:07:50 +1100

    HG-3139 cleaned up datalogger and phasemeter basic examples

--- examples/phasemeter_basic.py
+++ examples/phasemeter_basic.py
@@ -5,9 +5,9 @@
 # and log single-channel phase and [I,Q] data to a CSV file for a 10
 # second duration.
 #
-# (c) 2017 Liquid Instruments Pty. Ltd.
+# (c) 2019 Liquid Instruments Pty. Ltd.
 #
-from pymoku import *
+from pymoku import Moku
 from pymoku.instruments import Phasemeter
 import time
 
@@ -15,11 +15,11 @@ import time
 # Alternatively, use Moku.get_by_serial('#####') or Moku('192.168.###.###')
 m = Moku.get_by_name('Moku')
 
-# Deploy the new Phasemeter instrument to the Moku:Lab
-# Also, try to synchronise the Moku:Lab to an external 10MHz reference
-i = m.deploy_instrument(Phasemeter, use_external=True)
-
 try:
+	# Deploy the new Phasemeter instrument to the Moku:Lab
+	# Also, try to synchronise the Moku:Lab to an external 10MHz reference
+	i = m.deploy_instrument(Phasemeter, use_external=True)
+
 	# Set the Channel 1 seed frequency to 10MHz and a sample rate of ~30Hz
 	i.set_initfreq(1, 10e6)
 	i.set_samplerate('slow')
@@ -33,17 +33,22 @@ try:
 	i.start_data_log(duration=10, use_sd=True, ch1=True, ch2=False, filetype='csv')
 
 	# Track progress percentage of the data logging session
-	print("Waiting for completion.")
-	while i.progress_data_log() < 100:
-		time.sleep(1)
+	progress = 0
+	while progress < 100:
+		# Wait for the logging session to progress by sleeping 0.5sec
+		time.sleep(0.5)
+		# Get current progress percentage and print it out
+		progress = i.progress_data_log()
+		print("Progress {}%".format(progress))
 
 	# Upload the log file to the local directory
 	i.upload_data_log()
 	print("Uploaded log file to local directory.")
 
+	# Denote that we are done with the data logging session so resources may be cleand up
+	i.stop_data_log()
+
 except StreamException as e:
 	print("Error occured: %s" % e)
 finally:
-	# Denote that we are done with the data logging session so resources may be cleand up
-	i.stop_data_log()
 	m.close()
